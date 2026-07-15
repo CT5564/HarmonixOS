0304ec478c8748174a75613130f183473f30d005
CT5564 <joshuakeithvillanueva@gmail.com>
2026-07-15 23:10:25 +0800

    working ping

--- cogs/general.py
+++ cogs/general.py
@@ -1,13 +1,24 @@
 from discord.ext import commands
+from discord import app_commands
+
 
 class General(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command()
-    async def ping(self, ctx):
-        await ctx.send("Pong!")
+    @app_commands.command(
+        name="ping",
+        description="Check if Harmonix is online."
+    )
+    async def ping(self, interaction):
+
+        latency = round(self.bot.latency * 1000)
+
+        await interaction.response.send_message(
+            f"🏓 Pong!\nLatency: {latency} ms"
+        )
+
 
 async def setup(bot):
     await bot.add_cog(General(bot))
---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import asyncio
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
+
+    print(f"Logged in as {bot.user}")
+
+import os
+
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            print(f"Loaded {filename}")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
